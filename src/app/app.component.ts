60256e026038538553ff7f0e75e74a721851bb34
Nate Stuyvesant <nate@spotlight.ai>
2023-06-10 17:11:50 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular16-test';
+  options = {
+    title: 'Area (time series - natural curve)',
+    axes: {
+      bottom: {
+        title: '2019 Annual Sales Figures',
+        mapsTo: 'date',
+        scaleType: 'time'
+      },
+      left: {
+        mapsTo: 'value',
+        scaleType: 'linear'
+      }
+    },
+    curve: 'curveNatural',
+    height: '400px'
+  }
+
+  data = [
+    {
+      group: 'Dataset 1',
+      date: '2019-01-01T05:00:00.000Z',
+      value: 0
+    },
+    {
+      group: 'Dataset 1',
+      date: '2019-01-06T05:00:00.000Z',
+      value: -37312
+    },
+    {
+      group: 'Dataset 1',
+      date: '2019-01-08T05:00:00.000Z',
+      value: -22392
+    },
+    {
+      group: 'Dataset 1',
+      date: '2019-01-15T05:00:00.000Z',
+      value: -52576
+    },
+    {
+      group: 'Dataset 1',
+      date: '2019-01-19T05:00:00.000Z',
+      value: 20135
+    },
+    {
+      group: 'Dataset 2',
+      date: '2019-01-01T05:00:00.000Z',
+      value: 47263
+    },
+    {
+      group: 'Dataset 2',
+      date: '2019-01-05T05:00:00.000Z',
+      value: 14178
+    },
+    {
+      group: 'Dataset 2',
+      date: '2019-01-08T05:00:00.000Z',
+      value: 23094
+    },
+    {
+      group: 'Dataset 2',
+      date: '2019-01-13T05:00:00.000Z',
+      value: 45281
+    },
+    {
+      group: 'Dataset 2',
+      date: '2019-01-19T05:00:00.000Z',
+      value: -63954
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { ChartsModule } from '@carbon/charts-angular'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
